3ea5139b0ffbc967f44dbdf6a5b76328639cc087
Michael Ross Tarr <mrmrmtarr@gmail.com>
2022-08-23 21:18:47 +0200

    Updates

--- EventHandler.js
+++ EventHandler.js
@@ -1,99 +1,135 @@
+// Michael Tarr u20537833
+function getEventsBetweenDates(start, end, array) {
+    const filtered = array.filter(element => {
+        if (element.dateStart >= start && element.dateEnd <= end) {
+            return element;
+        }
+    })
+    return filtered;
+}
+
+function getByMonth(month, array) {
+    const filtered = array.filter(element => {
+        const split = element.dateStart.split('/');
+
+        var tMonth = month.toString();
+
+        if (tMonth.length === 1) {
+            tMonth = "0" + tMonth;
+        }
+
+        if (split[1] === tMonth) {
+            return element;
+        }
+    })
+    return filtered;
+}
+
+function getUniqueDateAndSort(array) {
+    var filtered = array;
+    filtered = filtered.sort((eventOne, eventTwo) => {
+        if (eventTwo.dateStart > eventOne.dateStart) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })
+
+    const startArr = [], endArr = [];
+    filtered = filtered.filter(item => { 
+        if(startArr.includes(item.dateStart) && endArr.includes(item.dateEnd)) {
+            return null;
+        } else {
+            startArr.includes(item.dateStart) ? null : startArr.push(item.dateStart);
+            endArr.includes(item.dateEnd) ? null : endArr.push(item.dateEnd);
+            return item;
+        }
+    });
+
+    return filtered;
+}
+
+function getSummary(arguments, array) {
+    if(arguments.length === 0) {
+        if(Array.isArray(array)) {
+            const filtered = array.map(event => {
+                var str = ``;
+                event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
+                return str;
+            })
+            return filtered;
+        }
+    } else if(arguments.length === 1 && Array.isArray(arguments[0])) {
+        const filtered = arguments[0].map(event => {
+            var str = ``;
+            event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
+            return str;
+        })
+        return filtered;
+    } else if(arguments.length > 1) {
+        const args = Array.from(arguments);
+        const filtered = args.map(event => {
+            var str = ``;
+            event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
+            return str;
+        })
+        return filtered;
+    } else {
+        return [];
+    }
+}
+
 class EventHandler {
     constructor(array) {
         this.events = array;
     }
 
     getEventsBetweenDates(start, end) {
-        const filtered = this.events.filter(element => {
-            if (element.dateStart >= start && element.dateEnd <= end) {
-                return element;
-            }
-        })
-        return filtered;
+        return getEventsBetweenDates(start, end, this.events);
     }
 
     getByMonth(month) {
-        const filtered = this.events.filter(element => {
-            const split = element.dateStart.split('/');
-
-            var tMonth = month.toString();
-
-            if (tMonth.length === 1) {
-                tMonth = "0" + tMonth;
-            }
-
-            if (split[1] === tMonth) {
-                return element;
-            }
-        })
-        return filtered;
+        return getByMonth(month, this.events);
     }
 
     getUniqueDateAndSort() {
-        var filtered = this.events;
-        filtered = filtered.sort((eventOne, eventTwo) => {
-            if (eventTwo.dateStart > eventOne.dateStart) {
-                return -1;
-            } else {
-                return 1;
-            }
-        })
-
-        const startArr = [], endArr = [];
-        filtered = filtered.filter(item => { 
-            if(startArr.includes(item.dateStart) && endArr.includes(item.dateEnd)) {
-                return null;
-            } else {
-                startArr.includes(item.dateStart) ? null : startArr.push(item.dateStart);
-                endArr.includes(item.dateEnd) ? null : endArr.push(item.dateEnd);
-                return item;
-            }
-        });
-
-        return filtered;
+        return getUniqueDateAndSort(this.events);
     }
 
     getSummary() {
-        if(arguments.length === 0) {
-            if(Array.isArray(this.events)) {
-                const filtered = this.events.map(event => {
-                    var str = ``;
-                    event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
-                    return str;
-                })
-                return filtered;
-            }
-        } else if(arguments.length === 1 && Array.isArray(arguments[0])) {
-            const filtered = arguments[0].map(event => {
-                var str = ``;
-                event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
-                return str;
-            })
-            return filtered;
-        } else if(arguments.length > 1) {
-            const args = Array.from(arguments);
-            const filtered = args.map(event => {
-                var str = ``;
-                event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
-                return str;
-            })
-            return filtered;
-        } else {
-            return [];
-        }
+        return getSummary(arguments, this.events);
     }
 }
 
-Array.prototype.getByMonth = function() {
-    console.log("hellooooooooooo");
+Array.prototype.getSummary = function() {
+    if(Array.isArray(this)) {
+        return getSummary(this);
+    } else {
+        return [];
+    }
+    
+};
+
+Array.prototype.getEventsBetweenDates = function(start, end) {
+    if(Array.isArray(this)) {
+        return getEventsBetweenDates(start, end, this);
+    } else {
+        return [];
+    }
 }
 
-Array.prototype.getSummary = function() {
-    // return EventHandler.getSummary(this);
-    const filtered = this.map(event => {
-        var str = ``;
-        event.dateStart === event.dateEnd ? str = `On ${event.dateStart}: ${event.name} (${event.description})` : str = `From ${event.dateStart} to ${event.dateEnd}: ${event.name} (${event.description})`;
-        return str;
-    })
-    return filtered;
-};
+Array.prototype.getByMonth = function(month) {
+    if(Array.isArray(this)) {
+        return getByMonth(month, this);
+    } else {
+        return [];
+    }
+}
+
+Array.prototype.getUniqueDateAndSort = function() {
+    if(Array.isArray(this)) {
+        return getUniqueDateAndSort(this);
+    } else {
+        return [];
+    } 
+}
